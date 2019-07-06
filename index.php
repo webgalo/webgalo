0a8d9a97c89ecc925b150a7b2c98100a7951963a
Ronaldo Cunha <admin@wgfinancas.com.br>
2019-07-06 11:11:03 -0300

    acrescentar novas telas

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ echo "<a href='kaprekar.php'>Kaprekar</a>".NEWLINE;
 echo "<a href='yahtzee.php'>Yahtzee</a>".NEWLINE;
 echo "<a href='smith.php'>smith</a>".NEWLINE;
 echo "<a href='netrunner.php'>Android:Netrunner functions</a>".NEWLINE;
+echo "<a href='freecell.php'>Freecell</a>".NEWLINE;
 
 
 
