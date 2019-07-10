84699bfa5f2bdcf1647d77ac507da18afc121366
Ronaldo Cunha <admin@wgfinancas.com.br>
2019-07-10 13:27:08 -0300

    quantidade de moedas e diametro

--- scraper.php
+++ scraper.php
@@ -23,12 +23,18 @@ require "vendor/autoload.php";
 use PHPHtmlParser\Dom;
 
 $dom = new Dom;
-$dom->loadFromUrl('https://en.numista.com/catalogue/index.php?mode=avance&p=1&l=&r=&e=bresil&d=&ru=&i=&ca=3&no=&m=&v=&t=&a=1889-2020&w=&dg=&f=&u=&g=&tb=y&tc=y&tn=y&tp=y&tt=y&te=y&cat=y&q=100&o=k');
+$dom->loadFromUrl('https://en.numista.com/catalogue/index.php?mode=avance&p=1&l=&r=&e=bresil&d=&ru=&i=&ca=3&no=&m=&v=&t=&a=1889-2020&w=&dg=&f=&u=&g=&tb=y&tc=y&tn=y&tp=y&tt=y&te=y&cat=y&q=10&o=k');
 
 //$html = $dom->outerHtml;
 
-$div = $dom->find('div [class="catalogue_navigation"]');
-echo $div."<BR>";
+$searchInfo = $dom->find('div [class="catalogue_navigation"]');
+
+$initial = strpos($searchInfo, "Display options</a>")+19;
+$final = strpos($searchInfo, " coins found.");
+$length = $final-$initial;
+$totalCoins = substr($searchInfo, $initial, $length);
+
+//echo $searchInfo."<BR>";
 
 
 $div = $dom->find('div [class="description_piece"]');
@@ -36,7 +42,14 @@ $div = $dom->find('div [class="description_piece"]');
 echo "<pre>";
 
 foreach ($div as $coin){
+
+    //"⌀ 34.1 mm"
+    $initial = strpos($coin, "&#8960;");
+    $final = strpos($coin, "mm");
+    $length = $final-$initial;
+    $diameter = substr($coin, $initial, $length);
     echo $coin."<BR>";
+    echo $diameter."<BR>";
 }
 
 echo "</pre>";
