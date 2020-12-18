46d1fde41b718fabdc7608b73d167189364a5041
Ronaldo Cunha <ronaldocunha@gmail.com>
2020-12-18 13:15:13 -0300

    alteração no metodo de escalonamento das imagens

--- teste.php
+++ teste.php
@@ -5,10 +5,10 @@ ini_set('display_errors', '1');
 ini_set('max_execution_time', '120');
 
 include_once("util.php");
-include("numista.php");
+//include("numista.php");
 
 //mergeImages("https://en.numista.com/catalogue/photos/portugal/5633-original.jpg", "https://en.numista.com/catalogue/photos/portugal/5634-original.jpg", 769);
 
-twit(55, '"washington quarter"');
+mergeImages("https://en.numista.com/catalogue/photos/etats-unis/4942-original.jpg", "https://en.numista.com/catalogue/photos/etats-unis/4943-original.jpg","x999");
 
 ?>
